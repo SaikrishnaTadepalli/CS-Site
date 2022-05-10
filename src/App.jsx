7425d1f855a2678069c24c609393612f78f25ec9
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2022-05-10 03:49:11 -0400

    Created Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css'
+import { useState } from 'react'; 
 
 function App() {
+  const [search, setSearch] = useState('');
+
+  function handleSearch(event) {
+    setSearch(event.target.value);
+  }
+
   return (
     <div className='App'>
+      <input 
+        type="text" 
+        placeholder='Search ...' 
+        onChange={handleSearch} 
+      />
       
     </div>
   );
