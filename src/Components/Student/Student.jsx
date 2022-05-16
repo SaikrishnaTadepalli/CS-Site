46e8ac60f500bfb3a842223abebe5839de83ba4d
vips11 <vipasha2002@gmail.com>
2022-05-16 12:49:06 -0400

    updated student card

--- src/Components/Student/Student.jsx
+++ src/Components/Student/Student.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './Student.css';
 
-export const Student = ({id, firstName, lastName, studentNum, program, term, link}) => {
+const Student = ({id, firstName, lastName, studentNum, program, term, link}) => {
   return (
     <div className="student" key={id}>
-        <p>
+        <p className="studentDetails">
             {firstName + " " + lastName + " | " + studentNum + " | " + program + " | " + term + " | Link: "}
             <a href={link}>
-                { (link.length <= 25) ? link : link.slice(0, 25) + "..." }
+                {(link.length <= 25) ? link : link.slice(0, 25) + "..." }
             </a>
         </p>
     </div>
